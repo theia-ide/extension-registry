fb4ecb121fa4d230d395b5b27d57b7f4ac1dd1ef
Jan Bicker <jan.bicker@typefox.io>
2019-10-17 14:51:51 +0000

    Initial commit
    Signed-off-by: Jan Bicker <jan.bicker@typefox.io>

---
+++ webui/configs/webpack.config.js
@@ -0,0 +1,51 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+
+    entry: [
+        './dev/index.tsx'
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dev')
+    },
+
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [{
+                    loader: 'ts-loader',
+                    options: {
+                        configFile: path.resolve(__dirname, 'examples.tsconfig.json')
+                    }
+                }]
+            },
+            {
+                test: /\.js$/,
+                use: ['source-map-loader'],
+                enforce: 'pre'
+            },
+            {
+                test: /\.css$/,
+                exclude: /\.useable\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    node : { fs: 'empty', net: 'empty' },
+
+    plugins: [
+        new webpack.WatchIgnorePlugin([
+            /\.js$/,
+            /\.d\.ts$/
+        ]),
+        new webpack.ProgressPlugin()
+    ]
+};
---
+++ webui/dev/index.tsx
@@ -0,0 +1,6 @@
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import {Foo} from '../src/foo';
+
+const node = document.getElementById('main');
+ReactDOM.render(<Foo/>, node);
